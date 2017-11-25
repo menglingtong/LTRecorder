ec9874c27583ab096a991ce1fad55b854853a2a3
孟令通 <ak-7755@qq.com>
2017-11-25 23:06:30 +0800

    addPlayerTimerObserver

--- LTRecorder/Framework/LTPlayer/LTPlayer.m
+++ LTRecorder/Framework/LTPlayer/LTPlayer.m
@@ -15,6 +15,39 @@
 
 @implementation LTPlayer
 
+static char* StatusChanged          = "StatusContext";
+static char* ItemChanged            = "CurrentItemContext";
+static char* PlaybackBufferEmpty    = "PlaybackBufferEmpty";
+static char* LoadedTimeRanges       = "LoadedTimeRanges";
+static char* PlaybackLikelyToKeepUp = "PlaybackLikelyToKeepUp";
+
+- (instancetype)init
+{
+    if ([super init]) {
+        // 1. 监听currentItem
+        [self addObserver:self forKeyPath:@"currentItem" options:NSKeyValueObservingOptionNew context:ItemChanged];
+        [self addPlayerTimerObserver];
+    }
+    return self;
+}
+
+- (void)addPlayerTimerObserver
+{
+    if (!self.isSendingPlayerInfo) {
+        __weak typeof(self)playerSelf = self;
+        _timeObserver = [self addPeriodicTimeObserverForInterval:CMTimeMake(1, 30) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+            if ([playerSelf.delegate respondsToSelector:@selector(player:didPlay:)]) {
+                [playerSelf.delegate player:playerSelf didPlay:time];
+            }
+        }];
+    }
+}
+
+- (BOOL)isSendingPlayerInfo
+{
+    return _timeObserver != nil;
+}
+
 + (LTPlayer *)player
 {
     return [LTPlayer new];
